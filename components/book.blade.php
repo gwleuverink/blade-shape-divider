67838b50b0ad885859aa6171bd29cf75a465f3b2
Willem Leuverink <willem@leuver.ink>
2020-08-16 02:53:12 +0200

    added 'book' divider

---
+++ components/book.blade.php
@@ -0,0 +1,11 @@
+@extends('shape-divider::_layout')
+
+@section('svg-content')
+
+    @if($invert)
+    <path d="M602.45,3.86h0S572.9,116.24,281.94,120H923C632,116.24,602.45,3.86,602.45,3.86Z" class="shape-fill"></path>
+    @else
+    <path d="M1200,0H0V120H281.94C572.9,116.24,602.45,3.86,602.45,3.86h0S632,116.24,923,120h277Z"></path>
+    @endif
+
+@endsection
