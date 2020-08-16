9893418d33000ed87e9343425453caa67cd23d68
Willem Leuverink <willem@leuver.ink>
2020-08-16 02:40:51 +0200

    added 'triangle' divider

---
+++ components/triangle.blade.php
@@ -0,0 +1,11 @@
+@extends('shape-divider::_layout')
+
+@section('svg-content')
+
+    @if($invert)
+    <path d="M598.97 114.72L0 0 0 120 1200 120 1200 0 598.97 114.72z"></path>
+    @else
+    <path d="M1200 0L0 0 598.97 114.72 1200 0z"></path>
+    @endif
+
+@endsection
